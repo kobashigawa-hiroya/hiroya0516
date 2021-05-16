5c097b76e71cf6ee37170cbf4c6b7c5e7cc3a2d4
hiroya <hiroya19890721@gmail.com>
2021-05-16 13:44:29 +0900

    webサイト作成

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(function() {
+$('#rec-slick').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	autoplay: true,
+	speed:1000,
+	autoplaySpeed: 5000,
+	arrows: false,
+	centerPadding: '100px',
+	centerMode: true,
+
+ });		 
+});
+
+
+
+// $(function() {
+// 	$('#rec-slick').slick({
+// 		infinite: true,
+// 		slidesToShow: 3,
+// 		slidesToScroll: 3,
+// 		autoplay: true,
+// 		speed:1000,
+// 		autoplaySpeed: 5000,
+// 		arrows: false,
+// 		centerPadding: '100px',
+// 		centerMode: true,
+	
+// 	 });		 
+// 	});
+	
+	
